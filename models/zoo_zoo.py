8b64767c13ea7c4e28b767a09fbfaf6033c980a9
Oleksandr <a2208033@institutmontilivi.cat>
2023-05-08 20:22:07 +0200

    sin errores y security ++

--- models/zoo_zoo.py
+++ models/zoo_zoo.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 class zoo_zoo(models.Model):
     _name = 'zoo.zoo'
-    id = fields.Intiger('ID', required=True)
+    id = fields.Integer('ID', required=True)
     nom = fields.Char('Nom', required=True)
     grandaria = fields.Char('Tamaño')
     ciutat = fields.Char('Ciutat')
