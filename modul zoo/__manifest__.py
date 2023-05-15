504de5699e0fd93e31b9ff22ca7db7ec874cafa4
Oleksandr <a2208033@institutmontilivi.cat>
2023-05-15 22:20:36 +0200

    manifest ++

--- modul zoo/__manifest__.py
+++ modul zoo/__manifest__.py
@@ -1,1 +1,11 @@
-
+{
+    'name': "modul zoo",
+    'summary': "zoo",
+    'description': """xd""",
+    'author': "Oleksandr Pomerantsev",
+    'license': "AGPL-3",
+    'website': "http://www.institutmontilivi.cat",
+    'category': 'Uncategorized',
+    'version': '11.0.1.0.0',
+    'data': ['security/ir.model.access.csv','views/views.xml'],
+}
