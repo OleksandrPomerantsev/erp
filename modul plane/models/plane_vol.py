f99f75afd86461d0e90510e9b5e84e66866847a0
Oleksandr <a2208033@institutmontilivi.cat>
2023-05-15 22:21:00 +0200

    t minuscula

--- modul plane/models/plane_vol.py
+++ modul plane/models/plane_vol.py
@@ -3,8 +3,8 @@ class plane_vol(models.Model):
     _name = 'plane.vol'
     name = fields.Char(compute='_get_name',string="Nom complet",readonly='True',store=False)
     passatgers = fields.Integer('Passatgers')
-    dataSortida = fields.DateTime('DataSortida')
-    dataArrivada = fields.DateTime('DataArrivada')
+    dataSortida = fields.Datetime('DataSortida')
+    dataArrivada = fields.Datetime('DataArrivada')
 
     def _get_name(self):
         for record in self:
