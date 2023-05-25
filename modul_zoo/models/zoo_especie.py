8e81a443e798a38b061235db186c132d36f1b9c6
Oleksandr <a2208033@institutmontilivi.cat>
2023-05-25 16:03:23 +0200

    cambios de vista +++

--- modul_zoo/models/zoo_especie.py
+++ modul_zoo/models/zoo_especie.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 class zoo_especie(models.Model):
     _name = 'zoo.especie'
     name = fields.Char(compute='_get_name',string="Nom complet",readonly='True',store=False)
-    perill = fields.Boolean('Perill de exincion (1 = si,0 = no)', required=True)
+    perill = fields.Boolean('Perill de exincion', required=True)
     familia = fields.Char('Familia')
     nomCientific = fields.Char('Nom cientific')
     nomVulgar = fields.Char('Nom vulgar')
