8f8b9cd864dc8447f182460b76e937a3e1b17af9
Oleksandr <a2208033@institutmontilivi.cat>
2023-05-29 21:01:16 +0200

    tengo todo pero el kanbas de plane no ba

--- modul_plane/models/plane_aeroport.py
+++ modul_plane/models/plane_aeroport.py
@@ -8,8 +8,8 @@ class plane_aeroport(models.Model):
     pais = fields.Char('Pais', required=True)
     latitud = fields.Float('Latitud', required=True)
     longitud = fields.Float('Longitud', required=True)
-    aeroportDesti_ids = fields.One2many('plane.vol','aeroportDesti',String='VolDesti')
-    aeroportOrigen_ids = fields.One2many('plane.vol','aeroportOrigen',String='VolOrigen')
+    aeroportDesti_ids = fields.One2many('plane.vol','aeroportDesti',String='aeroportDesti')
+    aeroportOrigen_ids = fields.One2many('plane.vol','aeroportOrigen',String='aeroportOrigen')
     image = fields.Image(string="Imagen")
     def _get_name(self):
         for record in self:
